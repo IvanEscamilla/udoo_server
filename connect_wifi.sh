36d73075ce82ab23a297131e10a8689ff75cd24d
IvanEscamilla <Naruxhinal2054192ws>
2016-11-20 17:02:34 -0600

    shell script for wifi connection

--- connect_wifi.sh
+++ connect_wifi.sh
@@ -9,41 +9,38 @@ echo "Tiene contraseña? [1 o 2]"
 echo "1. Si"
 echo "2. No"
 read contra
-
+echo ""
 if [ $contra -eq 1 ] ; then
 
+    echo "Que tipo de seguridad? [1 o 2]"
 	echo "1. WPA-PSK"
 	echo "2. WPA-EAP"
-    echo "Que tipo de seguridad? [1 o 2]"
     read tipo
     if [ $tipo -eq 1 ] ; then
-        $ key_mgmt = WPA-PSK
+        $key_mgmt = WPA-PSK
     else
-        $ key_mgmt = WPA-EAP
+        $key_mgmt = WPA-EAP
     fi
     
     echo "Introduce Password: "
     read pass
 
 else
-    
-    $ key_mgmt = NONE
-    
+    $key_mgmt = NONE
 fi
 
-wpa_cli -i wlan0
-sleep 2;
-
-for (( i=1; i<=5; i++ ))
-do
-    for (( j=1; j<=i;  j++ ))
-    do
-     echo -n "$i"
-    done
-    echo ""
-done
+output=$(nmcli device wifi connect "$SSID" password "$pass" iface wlan0 --timeout 10)
+wget -q --tries=5 --timeout=5 --spider http://google.com &> /dev/null # Is connected to Internet?
+if [[ $? -eq 0 ]]; then
+        echo "conectado a Internet tu ip: "; hostname -I; # Is connected to Internet
+        exit 0
+else
+        echo "Error. $output" # Anything goes wrong
+        exit 1
+fi
 
 #
 # ./ch.sh: vivek-tech.com to nixcraft.com referance converted using this tool
 # See the tool at http://www.nixcraft.com/uniqlinuxfeatures/tools/
 #
+
